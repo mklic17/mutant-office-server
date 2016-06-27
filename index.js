6c00dd1667774e3bc29aabfcc557ebcb03831c40
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-27 15:36:40 -0400

    Create a link to firebase for emails, successfully listens for a new email being added and sends the email to the person.

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var http = require('http');
 var firebase = require('firebase');
 var twilio = require('twilio');
 var dotenv = require('dotenv');
+var mailgun = require('mailgun-js');
 
 // Express Server Setup
 var app = express();
 var server = http.createServer(app);
 dotenv.load();
+
 
 //Autheniticate with firebase
 firebase.initializeApp({
@@ -20,6 +22,9 @@ var rootRef = firebase.database().ref();
 
 // Autheniticate with twilio
 var twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+// authentication with mailgun-js
+var mailgunClient = mailgun({apiKey: process.env.MAILGUN_KEY, domain: process.env.MAILGUN_DOMAIN});
 
 //listen for new tests being added
 var textsRef = rootRef.child('text');
@@ -36,6 +41,28 @@ textsRef.on('child_added', function(snapshot) {
         }
     });
 });
+
+
+//listen for new emails
+var emailsRef = rootRef.child('emails');
+emailsRef.on('child_added', function(snapshot){
+  var email = snapshot.val();
+
+  var emailData = {
+   from: '<postmaster@' + process.env.MAILGUN_DOMAIN +'>',
+   to: email.emailAddress,
+   subject: 'Welcome',
+   text: 'Mutant Office Hours, it is nice.'
+ };
+
+  mailgunClient.messages().send(emailData, function(error, body) {
+    console.log(body);
+    if (error) {
+      console.log(error);
+    }
+  });
+});
+
 
 server.listen(3030, function() {
   console.log('listening on http://localhost:3030');
